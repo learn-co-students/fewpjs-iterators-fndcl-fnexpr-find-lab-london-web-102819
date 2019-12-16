8ef16285291253280251f5c9d6cc18768cd899c0
andrew-lewell <andrew.lewell@gmail.com>
2019-12-16 17:52:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
 }
+
+function superbowlWin(array) {
+  let result = array.find(r => r.result === "W");
+  return !!result ? result.year : undefined;
+}
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+//   //...
+// ]
